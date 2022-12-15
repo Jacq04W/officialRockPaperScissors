f9a8a1890bcc185a2c6d6aecc9c7fc2ca1395de0
Jacquese <jacqwhitson@gmail.com>
2022-12-15 16:36:35 -0500

    Initial Commit

---
+++ officialRockPaperScissors/officialRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  officialRockPaperScissorsApp.swift
+//  officialRockPaperScissors
+//
+//  Created by Jacquese Whitson  on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct officialRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
